8b2d811725bf8e7d1d883420ab4f2636d77a3294
iorise <riofirmansyah1805@gmail.com>
2023-06-27 15:21:06 +0700

    adding types for data

---
+++ types/index.ts
@@ -0,0 +1,24 @@
+export interface WeatherData {
+  name: string;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    gust: number;
+    speed: number;
+  };
+}
